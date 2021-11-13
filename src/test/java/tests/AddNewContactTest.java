86ae02e8a77aaba0a7aa3ef146095e999d732d01
IrinaLerner <irina.bliudo14@gmail.com>
2021-11-13 16:06:15 +0200

    commit contact

--- src/test/java/tests/AddNewContactTest.java
+++ src/test/java/tests/AddNewContactTest.java
@@ -1,5 +1,15 @@
 package tests;
 
+import org.testng.annotations.BeforeMethod;
+
 public class AddNewContactTest extends TestBase{
+
+@BeforeMethod
+    public void precondition(){
+    if(app.getUser().islogged()){
+        app.getUser().
+    }
+
+}
 
 }
